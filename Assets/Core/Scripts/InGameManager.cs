5046409be70c8e0f32f10d0fe7f23a277636f4af
Kunwoo Kim <starryeev@gmail.com>
2026-01-03 16:25:56 +0900

    feat: GameContext 구현
    - GameContext 기초 기능 구현
    - InGameManager 기초 기능 구현
    - GameContextEvent 알림 및 구독 구현

--- Assets/Core/Scripts/InGameManager.cs
+++ Assets/Core/Scripts/InGameManager.cs
@@ -1,6 +1,138 @@
+using System;
 using UnityEngine;
+
+public class GameContext
+{
+    public enum Conclave
+    {
+        Dawn, Morning, Afternoon, Evening
+    }
+
+    public enum GameContextEvent
+    {
+        ConclaveStart, ConclaveEnd
+    }
+
+    int currentDay;
+    Conclave currentConclave;
+    float remainingTime;
+
+    public event Action<GameContextEvent> OnGameContextEvent;
+    public int CurrentDay => currentDay;
+    public Conclave CurrentConclave => currentConclave;
+    public float RemainingTime => remainingTime;
+
+    public void InitGameContext(int day=1, Conclave conclave=Conclave.Dawn)
+    {
+        currentDay = day;
+        currentConclave = conclave;
+        remainingTime = InGameManager.Instance.MaxConclaveTime;
+    }
+
+    public void AdvanceConclave()
+    {
+        if(currentConclave == Conclave.Evening)
+        {
+            currentConclave = Conclave.Dawn;
+            currentDay++;
+        }
+        else
+        {
+            currentConclave++;
+        }
+
+        remainingTime = InGameManager.Instance.MaxConclaveTime;
+
+        // 이벤트 알림
+        OnGameContextEvent?.Invoke(GameContextEvent.ConclaveStart);
+    }
+
+    public void Tick(float deltaTime)
+    {
+        if(remainingTime > 0)
+        {
+            remainingTime -= deltaTime;
+        }
+        else
+        {
+            AdvanceConclave();
+        }
+    }
+}
 
 public class InGameManager : MonoBehaviour
 {
+    [Header("게임 기본 설정")]
+    [Tooltip("한 콘클라베 당 시간(초)")]
+    [SerializeField] float maxConclaveTime = 60.0f;
 
+    // 싱글톤
+    public static InGameManager Instance { get; private set; }
+
+    // 멤버변수
+    private GameContext gameContext;
+
+    // 프로퍼티
+    public float MaxConclaveTime => maxConclaveTime;
+
+    void Awake()
+    {
+        // 싱글톤
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        // Awake 함수에 들어가야할 로직은 이 아래에
+        gameContext = new GameContext();
+        gameContext.OnGameContextEvent += HandleGameContextEvent;
+    }
+
+    void Start()
+    {
+        InitGame();
+
+    }
+
+    void Update()
+    {
+        gameContext.Tick(Time.deltaTime);
+    }
+
+    void InitGame()
+    {
+        gameContext.InitGameContext();
+    }
+
+    void HandleGameContextEvent(GameContext.GameContextEvent eventType)
+    {
+        switch(eventType)
+        {
+            // 임시 로직
+            case GameContext.GameContextEvent.ConclaveStart:
+                Debug.Log("콘클라베 시작");
+                break;
+            case GameContext.GameContextEvent.ConclaveEnd:
+                Debug.Log("콘클라베 끝");
+                break;
+        }
+    }
+
+    public int GetProgress()
+    {
+        CardinalManager cardinalManager = CardinalManager.Instance;
+
+        int result = 0;
+
+        int dayFactor = (gameContext.CurrentDay - 1) * 10;
+        int hpFactor = Mathf.RoundToInt(Mathf.Clamp((400 - cardinalManager.GetCardinalHpSum()) * 0.025f, 0, 10));
+        int pietyFactor = Mathf.RoundToInt(Mathf.Clamp(cardinalManager.GetCardinalPietySum() * 0.075f, 0, 30));
+
+        result = dayFactor + hpFactor + pietyFactor;
+
+        return result;
+    }
 }
