39ea8b819db6112b9e89c0bfc13c74a03bf0db3e
Kunwoo Kim <starryeev@gmail.com>
2026-01-11 14:48:12 +0900

    fix: 오타 수정

--- Assets/Item/Scripts/Item.cs
+++ Assets/Item/Scripts/Item.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 
 public enum ItemGrade { Common, Rare }
+public enum ItemExpirationType { Conclave, Day, Permanent, Special }
 
 public abstract class Item : ScriptableObject
 {
     public string itemID;
     public string itemName;
     public ItemGrade itemGrade;
+    public ItemExpirationType itemExpirationType;
 
     // 공통
     public virtual void OnAcquire() { }
     public virtual void OnRemove() { }
+
+    // 사용
+    public virtual void OnUse() { }
+
 }
